96c061ee31413be3fe0726a0b484032954f98107
ReverseGTLT <108753232+ReverseGTLT@users.noreply.github.com>
2022-10-24 16:06:57 +0300

    finish hw7 object

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const store = {
+  name: 'Sin nuts',
+  address: {
+    country: 'Ukraine',
+    city: 'Odessa',
+    street: 'Katerininska',
+    houseNumber: '29',
+  }
+}
+
+store.clients = {};
+
+store.clients['Viacheslav Koniev'] = {
+  quantity: 5,
+  bonuses: 5000,
+}
+store.clients['Татьяна Пыпко'] = {
+  quantity: 4,
+  bonuses: 4000,
+}
+store.clients['Ольга Тупчій'] = {
+  quantity: 3,
+  bonuses: 3000,
+}
+store.clients['Olena Demchyk'] = {
+  quantity: 2,
+  bonuses: 2000,
+}
+store.clients['Галина Мачинська'] = {
+  quantity: 1,
+  bonuses: 1000,
+}
